6534e6208e2ac03ce98d79e133d74730580d4930
Thiago <thiago@ISC.local>
2025-07-29 14:08:29 -0300

    Reformulação do layout para Desktop

--- src/components/geradorqrcode.tsx
+++ src/components/geradorqrcode.tsx
@@ -6,8 +6,11 @@ import logo from '../assets/logo.png';
 
 import { IoMdEye } from "react-icons/io";
 import { CiSaveDown2 } from "react-icons/ci";
-import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-import { FaUser } from "react-icons/fa";
+import { MdAlternateEmail, MdOutlineKeyboardArrowDown, MdOutlineSocialDistance, MdQrCodeScanner } from "react-icons/md";
+import { FaAddressCard, FaLink, FaMapPin, FaSms, FaUser, FaWifi } from "react-icons/fa";
+import { IoDocumentText } from "react-icons/io5";
+import { PiWarningCircleBold } from "react-icons/pi";
+import { RiErrorWarningFill } from "react-icons/ri";
 
 export const GeradordeQRCODE: React.FC = () => {
   const [text, setText] = useState("");
@@ -19,7 +22,6 @@ export const GeradordeQRCODE: React.FC = () => {
       const qrCodeURL = await QRCode.toDataURL(text);
       setQrCode(qrCodeURL);
 
-      const novoItem = { nome: text, qrCode: qrCodeURL };
       const novoValor = [...salvaQRCODE, qrCodeURL];
       setSalvaQRCODE(novoValor);
       localStorage.setItem('qrcode', JSON.stringify(novoValor));
@@ -37,9 +39,10 @@ export const GeradordeQRCODE: React.FC = () => {
 
   return (
     <section className="flex flex-col justify-center items-center content-center">
-      <header className="bg-white w-full justify-center items-center flex p-5 mb-5">
+
+      <header className="bg-white w-full h-20 items-center justify-center flex p-5 mb-5">
         <img className="w-12 h-12 rounded-lg" src={logo}></img>
-        <nav className="flex justify-between items-center w-8/12">
+        <nav className="flex items-center w-8/12">
           <ul className="flex items-center">
             <li className="text-gray-500 text-lg font-semibold text-center p-5">
               <a className="hover:underline transition-all duration-300 flex justify-center items-center text-blue-500 underline" href="#">Código QR&nbsp;<MdOutlineKeyboardArrowDown /></a>
@@ -54,24 +57,52 @@ export const GeradordeQRCODE: React.FC = () => {
               <a className="hover:underline transition-all duration-300" href="#">Contacte-nos</a>
             </li>
           </ul>
-          <div className="flex gap-5 items-center">
-            <span className="hover:underline transition-all duration-300 font-semibold hover:text-blue-500">
-              <FaUser className="w-5 h-5 cursor-pointer" /></span>
-            <button
-              className="bg-blue-600 text-white rounded-lg w-60 h-15 flex justify-center items-center hover:bg-blue-700 transition-all duration-300">
-              Comece a avaliação gratuita
-            </button>
-          </div>
         </nav>
+        <div className="flex gap-5 items-center">
+          <span className="hover:underline transition-all duration-300 font-semibold hover:text-blue-500">
+            <FaUser className="w-5 h-5 cursor-pointer" /></span>
+          <button
+            className="bg-blue-600 text-white rounded-lg w-60 h-15 flex justify-center items-center hover:bg-blue-700 transition-all duration-300">
+            Comece a avaliação gratuita
+          </button>
+        </div>
       </header>
 
-      <div className="w-9/12 p-5 leading-0.75">
+
+
+
+
+      <div className="w-8/12 p-5 leading-0.75">
         <h1 className="text-4xl font-bold text-gray-800 mb-5">Gerador de QR Code</h1>
         <p className="text-2xl text-gray-600 mb-5">Criador de QR codes personalizados</p>
       </div>
 
-      <div className="flex w-9/12 p-5 h-130 gap-5 justify-center items-center flex-col lg:flex-row">
-        <div className="order-2 bg-white p-4 rounded-lg">
+
+      <div className="bg-white w-8/12 h-full p-5 rounded-lg flex items-center gap-5 flex-wrap justify-center">
+        <ul className="flex gap-10 flex-wrap justify-center">
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <FaLink className="w-8 h-8 text-blue-500" />URL</li>
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <FaAddressCard className="w-8 h-8 text-blue-500" />vCard</li>
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <IoDocumentText className="w-8 h-8 text-blue-500" />Documento</li>
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <MdAlternateEmail className="w-8 h-8 text-blue-500" />E-mail</li>
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <FaWifi className="w-8 h-8 text-blue-500" />Wifi</li>
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <MdOutlineSocialDistance className="w-8 h-8 text-blue-500" />Social</li>
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <FaSms className="w-8 h-8 text-blue-500" />SMS</li>
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+            <FaMapPin className="w-8 h-8 text-blue-500" />Localização</li>
+        </ul>
+      </div>
+
+
+      <div className="flex w-8/12 pt-5 h-140 gap-5 justify-center items-center flex-col lg:flex-row">
+
+        <div className="order-2 bg-white h-full p-4 rounded-lg flex flex-col items-center">
           <div className="w-80 h-80 bg-white flex justify-center items-center rounded-lg">
             {!qrCode && (
               <>
@@ -87,37 +118,45 @@ export const GeradordeQRCODE: React.FC = () => {
               </>
             )}
           </div>
-
           <button
             className="bg-blue-600 text-white rounded-lg w-full h-15 mt-5 flex justify-center items-center hover:bg-blue-700 transition-all duration-300"
             onClick={GerarQrCode}>
             <IoMdEye className="w-12 h-12" />&nbsp; Gerar QRCode
           </button>
-          
           <div className="flex justify-center items-center m-5 gap-3">
             <button
               className="w-50 flex justify-center items-center border-1 bg-red-400 rounded-full p-2 text-white hover:bg-red-500">
               Download&nbsp;<CiSaveDown2 className="w-5 h-5" />
             </button>
-            <span className="m-0 p-0 cursor-pointer hover:text-blue-300">Duvidas?</span>
+            <span className="w-30 flex justify-center items-center border-1 bg-green-500 rounded-full p-2 text-white hover:bg-green-600 cursor-pointer">Imprimir&nbsp;<MdQrCodeScanner className="w-5 h-5"/></span>
+          </div>
+        </div>
+
+        <div className="flex w-full h-full bg-white rounded-lg m-0 p-0 flex-col">
+          <div className="w-full h-15 bg-blue-500 rounded-t-lg flex justify-center items-center">
+            <h2 className="text-white text-2xl font-semibold">Parametros do QR Code</h2>
           </div>
 
+          <div className="flex flex-col w-full h-full items-start p-5 gap-5">
+            <span className="text-4xl font-bold text-gray-800 ml-5">URL ou pagina da web</span>
+            <div className="flex flex-col w-9/12 lg:w-6/12 h-30 bg-white justify-center items-center rounded-lg mt-5">
+              <label className="font-bold text-gray-600 w-11/12" htmlFor="url">Insira o link:</label>
+              <input
+                className="bg-blue-100 w-11/12 rounded-lg h-15 text-blue-500 font-semibold p-3 ative:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                type="text"
+                id="url"
+                placeholder="Ex: https://www.exemplo.com"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+            </div>
+            <p className="ml-5 flex text-gray-700 gap-2 w-140">
+              <RiErrorWarningFill className="w-13 h-13 text-blue-800" />
+              Recomendamos que antes de clicar em criar código QR verifique que a informação do campo link esteja preenchida corretamente.
+            </p>
+          </div>
         </div>
 
-        <div className="flex w-full h-full bg-white rounded-lg">
-        </div>
-
-      </div>
-      <div className="flex flex-col w-8/12 lg:w-6/12 h-30 bg-white justify-center items-center rounded-lg mt-5">
-        <label className="font-semibold text-gray-600 w-11/12" htmlFor="url">URL</label>
-        <input
-          className="bg-blue-100 w-11/12 rounded-lg h-15 text-blue-500 font-semibold p-3 ative:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          type="text"
-          id="url"
-          placeholder="Link do site ou texto"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
       </div>
 
       <h3 className="mb-5 mt-10">QRcode salvos:</h3>
