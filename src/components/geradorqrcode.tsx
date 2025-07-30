35486483e2a0d1f365ce2621c71c59acb361ee0b
Thiago-Nascimento-2000 <thiago.s2nascimentoo@gmail.com>
2025-07-29 22:32:12 -0300

    Finalização do Layout para Desktop e função de exclusão de qrcode

--- src/components/geradorqrcode.tsx
+++ src/components/geradorqrcode.tsx
@@ -7,10 +7,10 @@ import logo from '../assets/logo.png';
 import { IoMdEye } from "react-icons/io";
 import { CiSaveDown2 } from "react-icons/ci";
 import { MdAlternateEmail, MdOutlineKeyboardArrowDown, MdOutlineSocialDistance, MdQrCodeScanner } from "react-icons/md";
-import { FaAddressCard, FaLink, FaMapPin, FaSms, FaUser, FaWifi } from "react-icons/fa";
+import { FaAddressCard, FaLink, FaMapPin, FaPencilAlt, FaSms, FaUser, FaWifi } from "react-icons/fa";
 import { IoDocumentText } from "react-icons/io5";
-import { PiWarningCircleBold } from "react-icons/pi";
 import { RiErrorWarningFill } from "react-icons/ri";
+import { TbTrashXFilled } from "react-icons/tb";
 
 export const GeradordeQRCODE: React.FC = () => {
   const [text, setText] = useState("");
@@ -22,12 +22,18 @@ export const GeradordeQRCODE: React.FC = () => {
       const qrCodeURL = await QRCode.toDataURL(text);
       setQrCode(qrCodeURL);
 
-      const novoValor = [...salvaQRCODE, qrCodeURL];
+      const novoValor = [...salvaQRCODE, { nome: text, qrCode: qrCodeURL }];
       setSalvaQRCODE(novoValor);
       localStorage.setItem('qrcode', JSON.stringify(novoValor));
     } catch (error) {
       console.log("Erro ao gerar QR Code:", error);
     }
+  };
+
+  const RemoverQrCode = (indexParaRemover: number) => {
+    const novaLista = salvaQRCODE.filter((_, index) => index !== indexParaRemover);
+    setSalvaQRCODE(novaLista);
+    localStorage.setItem('qrcode', JSON.stringify(novaLista));
   };
 
   useEffect(() => {
@@ -78,9 +84,9 @@ export const GeradordeQRCODE: React.FC = () => {
       </div>
 
 
-      <div className="bg-white w-8/12 h-full p-5 rounded-lg flex items-center gap-5 flex-wrap justify-center">
-        <ul className="flex gap-10 flex-wrap justify-center">
-          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
+      <div className="bg-white w-8/12 h-full p-5 rounded-lg flex items-center flex-wrap justify-center">
+        <ul className="flex flex-wrap justify-center">
+          <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center -translate-y-1 shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
             <FaLink className="w-8 h-8 text-blue-500" />URL</li>
           <li className="cursor-pointer w-30 h-25 text-gray-800 font-semibold flex-col text-xl justify-center items-center hover:-translate-y-1 hover:shadow-lg transition-all duration-300 flex gap-5 rounded-lg">
             <FaAddressCard className="w-8 h-8 text-blue-500" />vCard</li>
@@ -104,6 +110,7 @@ export const GeradordeQRCODE: React.FC = () => {
 
         <div className="order-2 bg-white h-full p-4 rounded-lg flex flex-col items-center">
           <div className="w-80 h-80 bg-white flex justify-center items-center rounded-lg">
+
             {!qrCode && (
               <>
                 <img className="w-60 h-60 opacity-50" src={imagemqrcode}></img>
@@ -117,6 +124,7 @@ export const GeradordeQRCODE: React.FC = () => {
                 />
               </>
             )}
+
           </div>
           <button
             className="bg-blue-600 text-white rounded-lg w-full h-15 mt-5 flex justify-center items-center hover:bg-blue-700 transition-all duration-300"
@@ -125,10 +133,10 @@ export const GeradordeQRCODE: React.FC = () => {
           </button>
           <div className="flex justify-center items-center m-5 gap-3">
             <button
-              className="w-50 flex justify-center items-center border-1 bg-red-400 rounded-full p-2 text-white hover:bg-red-500">
+              className="w-40 flex justify-center items-center border-1 bg-red-400 rounded-full p-2 text-white hover:bg-red-500">
               Download&nbsp;<CiSaveDown2 className="w-5 h-5" />
             </button>
-            <span className="w-30 flex justify-center items-center border-1 bg-green-500 rounded-full p-2 text-white hover:bg-green-600 cursor-pointer">Imprimir&nbsp;<MdQrCodeScanner className="w-5 h-5"/></span>
+            <span className="w-30 flex justify-center items-center border-1 bg-green-500 rounded-full p-2 text-white hover:bg-green-600 cursor-pointer">Imprimir&nbsp;<MdQrCodeScanner className="w-5 h-5" /></span>
           </div>
         </div>
 
@@ -138,11 +146,11 @@ export const GeradordeQRCODE: React.FC = () => {
           </div>
 
           <div className="flex flex-col w-full h-full items-start p-5 gap-5">
-            <span className="text-4xl font-bold text-gray-800 ml-5">URL ou pagina da web</span>
-            <div className="flex flex-col w-9/12 lg:w-6/12 h-30 bg-white justify-center items-center rounded-lg mt-5">
-              <label className="font-bold text-gray-600 w-11/12" htmlFor="url">Insira o link:</label>
+            <span className="text-4xl font-bold text-gray-800">URL ou pagina da web</span>
+            <div className="flex flex-col lg:w-10/12 h-30 bg-white justify-center items-center rounded-lg">
+              <label className="font-bold text-gray-600 w-full" htmlFor="url">Insira o link:</label>
               <input
-                className="bg-blue-100 w-11/12 rounded-lg h-15 text-blue-500 font-semibold p-3 ative:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="bg-blue-100 w-full h-15 rounded-lg text-blue-500 font-semibold p-3 ative:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 type="text"
                 id="url"
                 placeholder="Ex: https://www.exemplo.com"
@@ -150,7 +158,7 @@ export const GeradordeQRCODE: React.FC = () => {
                 onChange={(e) => setText(e.target.value)}
               />
             </div>
-            <p className="ml-5 flex text-gray-700 gap-2 w-140">
+            <p className="flex text-gray-700 gap-2 w-auto">
               <RiErrorWarningFill className="w-13 h-13 text-blue-800" />
               Recomendamos que antes de clicar em criar código QR verifique que a informação do campo link esteja preenchida corretamente.
             </p>
@@ -159,20 +167,53 @@ export const GeradordeQRCODE: React.FC = () => {
 
       </div>
 
-      <h3 className="mb-5 mt-10">QRcode salvos:</h3>
-      <ul className="flex flex-wrap justify-center">
-        {
-          salvaQRCODE.map((valor, index) => (
-            <li key={index} className="list-none">
-              <p className="list-none text-center m-2">{valor.nome}</p>
-              <img
-                className="bg-blue-400 w-50 h-50 rounded-lg p-2 m-1 gap-1"
-                src={valor}
-                alt={`QR de ${valor.nome}`} />
-            </li>
-          ))
-        }
-      </ul>
+      <section className="bg-white w-8/12 rounded-lg mt-5 mb-5 p-5">
+        <div className=" rounded-lg p-5">
+          <ul className="flex flex-col flex-wrap justify-center">
+            {salvaQRCODE.map((valor, index) => (
+              <li key={index} className="list-none flex m-2 p-5 rounded-lg bg-gray-100">
+                <img
+                  className="bg-blue-500 rounded-lg p-2 m-1 gap-1"
+                  src={valor.qrCode}
+                  alt={`QR ${valor.nome}`} />
+
+
+                <div className="flex flex-col justify-center items-center ml-5">
+                  <div className="flex flex-col w-full justify-center">
+                    <button
+                      className="w-40 flex justify-center items-center border-1 bg-red-400 rounded-full p-2 text-white hover:bg-red-500">
+                      Download&nbsp;<CiSaveDown2 className="w-5 h-5" />
+                    </button>
+
+                    <div className="flex justify-center gap-2">
+                      <button>
+                        <FaPencilAlt className="text-blue-700 w-5 h-5 m-2" />
+                      </button>
+                      <button>
+                        <CiSaveDown2 className="text-blue-700 w-5 h-5 m-2" />
+                      </button>
+                      <button>
+                        <MdQrCodeScanner className="text-blue-700 w-5 h-5 m-2" />
+                      </button>
+                    </div>
+                  </div>
+
+
+                </div>
+                <div className="flex flex-col w-full justify-center items-end gap-2">
+                  <a href={valor.nome} className="list-none text-center text-gray-700 underline m-2 text-2xl font-semibold">{valor.nome}</a>
+                  <button
+                    onClick={() => RemoverQrCode(index)}
+                    className="bg-red-400 w-30 h-10 rounded-full flex justify-center items-center font-semibold text-white hover:bg-red-500"
+                  >
+                    Apagar&nbsp;<TbTrashXFilled className="w-5 h-5" />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     </section>
   );
 };
